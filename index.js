c310cf073501cff1a4e6d7d744e4e8517a0f5a25
tt-le <tienthanh.le@mail.utoronto.ca>
2020-06-17 18:56:02 -0400

    ejs views

---
+++ index.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.listen(PORT, ()=>{
+    console.log("server started");
+});
