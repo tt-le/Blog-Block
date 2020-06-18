025195b43dd791ec83e78b2c6de031944ff9fb82
tt-le <tienthanh.le@mail.utoronto.ca>
2020-06-17 21:46:10 -0400

    mongo connection, schema + routes

--- index.js
+++ index.js
@@ -1,24 +1,95 @@
 //jshint esversion:6
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const _ = require('lodash');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render('home');
+//DATABASE
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(PORT, ()=>{
-    console.log("server started");
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+//Lorum Ipsum content
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+app.get("/", (req, res) => {
+  Post.find({}, (err, posts) => {
+    res.render("home", { homeContent: homeStartingContent, POSTS: posts });
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.get("/posts/:postID", (req, res) => {
+  const requestedId = req.params.postID;
+  console.log("the req id" + requestedId);
+
+  Post.findById(req.params.postID, (err, requestedPost) => {
+    res.render("post", {
+      title: requestedPost.title,
+      body: requestedPost.body,
+    });
+  });
+});
+
+app.post("/compose", (req, res) => {
+  const newPost = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody,
+    date: date.getDate(),
+  });
+  newPost.save();
+  res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log("server started");
 });
